77678bf5bd9857560230f1565bc166d2aad07676
Richard Lander <lander2k2@protonmail.com>
2018-04-19 11:18:24 -0400

    Deploy dedicated VPC for k8s cluster

--- infra/masters.tf
+++ infra/masters.tf
@@ -3,7 +3,8 @@ variable "master_ami" {}
 variable "master_type" {}
 
 resource "aws_security_group" "master_sg" {
-  name = "master_sg"
+  name   = "master_sg"
+  vpc_id = "${aws_vpc.k8s_cluster.id}"
 
   ingress {
     from_port   = 6443
@@ -15,13 +16,13 @@ resource "aws_security_group" "master_sg" {
     from_port   = 2379
     to_port     = 2380
     protocol    = "TCP"
-    cidr_blocks = ["172.31.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
   ingress {
     from_port   = 10250
     to_port     = 10255
     protocol    = "TCP"
-    cidr_blocks = ["172.31.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
   ingress {
     from_port   = 22
@@ -38,61 +39,69 @@ resource "aws_security_group" "master_sg" {
   }
 }
 
+resource "aws_security_group" "master_lb_sg" {
+  name   = "master_lb_sg"
+  vpc_id = "${aws_vpc.k8s_cluster.id}"
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "master0_node" {
-  count                  = 1
-  ami                    = "${var.master0_ami}"
-  instance_type          = "${var.master_type}"
-  vpc_security_group_ids = ["${aws_security_group.master_sg.id}"]
-  key_name               = "${var.key_name}"
+  count                       = 1
+  ami                         = "${var.master0_ami}"
+  instance_type               = "${var.master_type}"
+  subnet_id                   = "${aws_subnet.k8s_cluster0.id}"
+  vpc_security_group_ids      = ["${aws_security_group.master_sg.id}"]
+  key_name                    = "${var.key_name}"
+  associate_public_ip_address = "true"
+  depends_on                  = ["aws_internet_gateway.k8s_cluster"]
   tags {
     Name = "master0"
   }
 }
 
 resource "aws_instance" "master_node" {
-  count                  = 2
-  ami                    = "${var.master_ami}"
-  instance_type          = "${var.master_type}"
-  vpc_security_group_ids = ["${aws_security_group.master_sg.id}"]
-  key_name               = "${var.key_name}"
+  count                       = 2
+  ami                         = "${var.master_ami}"
+  instance_type               = "${var.master_type}"
+  subnet_id                   = "${aws_subnet.k8s_cluster0.id}"
+  vpc_security_group_ids      = ["${aws_security_group.master_sg.id}"]
+  key_name                    = "${var.key_name}"
+  associate_public_ip_address = "true"
+  depends_on                  = ["aws_internet_gateway.k8s_cluster"]
   tags {
     Name = "master"
   }
 }
 
 resource "aws_elb" "api_elb_external" {
-  subnets = ["subnet-2c5ee157", "subnet-341e6f5d", "subnet-b7ed22fa"]
-  internal = false
-  instances = ["${aws_instance.master0_node.id}", "${aws_instance.master_node.*.id}"]
-  cross_zone_load_balancing = true
+  subnets                   = ["${aws_subnet.k8s_cluster0.id}"]
+  internal                  = "false"
+  instances                 = ["${aws_instance.master0_node.id}", "${aws_instance.master_node.*.id}"]
+  security_groups           = ["${aws_security_group.master_lb_sg.id}"]
+  cross_zone_load_balancing = "true"
 
   listener {
-    instance_port = 6443
+    instance_port     = 6443
     instance_protocol = "TCP"
-    lb_port = 6443
-    lb_protocol = "TCP"
+    lb_port           = 6443
+    lb_protocol       = "TCP"
   }
 
   tags {
     Name = "kubernetes API external"
-  }
-}
-
-resource "aws_elb" "api_elb_internal" {
-  subnets = ["subnet-2c5ee157", "subnet-341e6f5d", "subnet-b7ed22fa"]
-  internal = true
-  instances = ["${aws_instance.master0_node.id}", "${aws_instance.master_node.*.id}"]
-  cross_zone_load_balancing = true
-
-  listener {
-    instance_port = 6443
-    instance_protocol = "TCP"
-    lb_port = 6443
-    lb_protocol = "TCP"
-  }
-
-  tags {
-    Name = "kubernetes API internal"
   }
 }
 
@@ -104,7 +113,7 @@ output "master_ep" {
   value = "${aws_instance.master_node.*.public_dns}"
 }
 
-output "api_internal_lb_ep" {
-  value = "${aws_elb.api_elb_internal.dns_name}"
+output "api_lb_ep" {
+  value = "${aws_elb.api_elb_external.dns_name}"
 }
 
--- infra/workers.tf
+++ infra/workers.tf
@@ -2,25 +2,26 @@ variable "worker_ami" {}
 variable "worker_type" {}
 
 resource "aws_security_group" "worker_sg" {
-  name = "worker_sg"
+  name   = "worker_sg"
+  vpc_id = "${aws_vpc.k8s_cluster.id}"
 
   ingress {
     from_port   = 10250
     to_port     = 10250
     protocol    = "TCP"
-    cidr_blocks = ["172.31.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
   ingress {
     from_port   = 10250
     to_port     = 10250
     protocol    = "TCP"
-    cidr_blocks = ["172.31.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
   ingress {
     from_port   = 30000
     to_port     = 32767
     protocol    = "TCP"
-    cidr_blocks = ["172.31.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
   ingress {
     from_port   = 22
@@ -38,11 +39,14 @@ resource "aws_security_group" "worker_sg" {
 }
 
 resource "aws_instance" "worker_node" {
-  count                  = 1
-  ami                    = "${var.worker_ami}"
-  instance_type          = "${var.worker_type}"
-  vpc_security_group_ids = ["${aws_security_group.worker_sg.id}"]
-  key_name               = "${var.key_name}"
+  count                       = 1
+  ami                         = "${var.worker_ami}"
+  instance_type               = "${var.worker_type}"
+  subnet_id                   = "${aws_subnet.k8s_cluster0.id}"
+  vpc_security_group_ids      = ["${aws_security_group.worker_sg.id}"]
+  key_name                    = "${var.key_name}"
+  associate_public_ip_address = "true"
+  depends_on                  = ["aws_internet_gateway.k8s_cluster"]
   tags {
     Name = "worker"
   }
