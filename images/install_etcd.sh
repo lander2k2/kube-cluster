8a41f37b0c2e50e99dc1dee6250f5d0d324969c9
Richard Lander <lander2k2@protonmail.com>
2018-05-21 11:59:20 -0400

    Switch back to etcd download with wget

--- images/install_etcd.sh
+++ images/install_etcd.sh
@@ -2,17 +2,14 @@
 
 ETCD_VERSION=v3.1.10
 
-sudo apt-get update
-sudo apt-get -y upgrade
-
 sudo curl -o /usr/local/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
 sudo curl -o /usr/local/bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
 sudo chmod +x /usr/local/bin/cfssl*
 
-#curl -sSL https://github.com/coreos/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz | sudo tar -xzv --strip-components=1 -C /usr/local/bin/
-#rm -rf etcd-$ETCD_VERSION-linux-amd64*
-
+wget https://github.com/coreos/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -O /tmp/etcd.tar.gz
 tar xvf /tmp/etcd.tar.gz -C /tmp
 sudo mv /tmp/etcd-$ETCD_VERSION-linux-amd64/etcd /usr/local/bin/
 sudo mv /tmp/etcd-$ETCD_VERSION-linux-amd64/etcdctl /usr/local/bin/
+rm /tmp/etcd.tar.gz
+rm -rf /tmp/etcd-$ETCD_VERSION-linux-amd64
 
