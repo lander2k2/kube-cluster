801658575bd3c5d321e8d0c1e25f5a08622a9d18
Richard Lander <lander2k2@protonmail.com>
2018-05-25 08:57:40 -0400

    Use binary upload for k8s install on centos

--- images/install_k8s_centos.sh
+++ images/install_k8s_centos.sh
@@ -10,16 +10,14 @@ sudo mv /tmp/selinux_config /etc/selinux/config
 sudo yum install -y docker
 sudo systemctl enable docker
 
-sudo tee -a /etc/yum.repos.d/kubernetes.repo <<EOF
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-sudo yum install -y kubelet-1.9.7-0 kubeadm-1.9.7-0 kubectl-1.9.7-0
+cd /tmp; tar xvf /tmp/kubernetes-node-linux-amd64.tar.gz
+sudo mv /tmp/kubernetes/node/bin/kubelet /usr/bin/
+sudo mv /tmp/kubernetes/node/bin/kubectl /usr/local/bin/
+sudo mv /tmp/kubernetes/node/bin/kubeadm /usr/local/bin/
+rm /tmp/kubernetes-node-linux-amd64.tar.gz
+rm -rf /tmp/kubernetes
+sudo mv /tmp/kubelet.service /lib/systemd/system/
+sudo mkdir /etc/systemd/system/kubelet.service.d/
 sudo systemctl enable kubelet
 
 sudo curl -o /usr/local/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
