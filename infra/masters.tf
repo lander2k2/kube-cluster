6a1d2c9dce9a230946728b87f71ae821844bb14b
Richard Lander <lander2k2@protonmail.com>
2018-06-13 11:36:57 -0400

    Refine node IAM policies

--- infra/masters.tf
+++ infra/masters.tf
@@ -36,35 +36,16 @@ resource "aws_iam_role_policy" "master_policy" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Effect": "Allow",
-      "Action": "ec2:Describe*",
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": "ec2:AttachVolume",
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": "ec2:DetachVolume",
-      "Resource": "*"
-    },
-    {
-      "Action": "elasticloadbalancing:*",
-      "Resource": "*",
-      "Effect": "Allow"
-    },
-    {
       "Action": [
-        "ecr:GetAuthorizationToken",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:GetRepositoryPolicy",
-        "ecr:DescribeRepositories",
-        "ecr:ListImages",
-        "ecr:BatchGetImage"
-      ],
+        "ec2:AttachVolume",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteVolume",
+        "ec2:Describe*",
+        "ec2:DescribeInstances",
+        "ec2:DetachVolume",
+        "elasticloadbalancing:*"
+        ],
       "Resource": "*",
       "Effect": "Allow"
     },
@@ -79,6 +60,19 @@ resource "aws_iam_role_policy" "master_policy" {
       "Action" : [
         "autoscaling:DescribeAutoScalingGroups",
         "autoscaling:DescribeAutoScalingInstances"
+      ],
+      "Resource": "*",
+      "Effect": "Allow"
+    },
+    {
+      "Action" : [
+        "ec2:CreateSecurityGroup",
+        "ec2:DescribeSecurityGroups",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:AuthorizeSecurityGroupEgress",
+        "ec2:DeleteSecurityGroup",
+        "ec2:RevokeSecurityGroupEgress",
+        "ec2:RevokeSecurityGroupIngress"
       ],
       "Resource": "*",
       "Effect": "Allow"
